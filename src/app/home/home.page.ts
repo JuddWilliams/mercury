053afc63c1497012ff9e127a34d737f1b2473092
Judd Williams <jwilliams@smartgistics.com>
2022-10-26 17:53:31 -0400

    init changes

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  currentJoke: string;
+  count = 0;
+  constructor(public http: HttpClient) {}
 
-  constructor() {}
+  ngOnInit() {
+    this.getJoke();
 
+    const i = setInterval(() => {
+      this.getJoke();
+    }, 10000);
+  }
+
+  public getJoke(): void {
+    console.log('getJoke');
+
+    this.getJokeFromAPI().subscribe((res) => {
+      console.log('getJoke', res);
+
+      this.count++;
+      this.currentJoke = `${res.setup} ... ${res.punchline}`;
+    });
+
+    return;
+  }
+
+  getJokeFromAPI(): Observable<any> {
+    return this.http.get('https://official-joke-api.appspot.com/jokes/random');
+  }
 }
